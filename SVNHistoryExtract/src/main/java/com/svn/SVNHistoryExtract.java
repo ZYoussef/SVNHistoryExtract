66073c311a10f9c0ad0854a7e9bd1cc6bc72f436
ZYoussef <36228102+ZYoussef@users.noreply.github.com>
2020-01-02 15:39:56 +0100

    update svn extract
    extract data from url svn and generate text file with the correspond revision de release

--- SVNHistoryExtract/src/main/java/com/svn/SVNHistoryExtract.java
+++ SVNHistoryExtract/src/main/java/com/svn/SVNHistoryExtract.java
@@ -1,151 +1,120 @@
 package com.svn;
 
-
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.Iterator;
-import java.util.Locale;
+import java.util.List;
 import java.util.Properties;
-import java.util.Set;
 
 import javax.swing.JOptionPane;
 
-import jxl.Workbook;
-import jxl.WorkbookSettings;
-import jxl.read.biff.BiffException;
-import jxl.write.Label;
-import jxl.write.WritableCell;
-import jxl.write.WritableSheet;
-import jxl.write.WritableWorkbook;
-import jxl.write.WriteException;
-import jxl.write.biff.RowsExceededException;
-
 import org.tmatesoft.svn.core.SVNLogEntry;
-import org.tmatesoft.svn.core.SVNLogEntryPath;
 import org.tmatesoft.svn.core.SVNURL;
 import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
 import org.tmatesoft.svn.core.io.SVNRepository;
 import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
-import org.tmatesoft.svn.core.wc.*;
+import org.tmatesoft.svn.core.wc.SVNClientManager;
+import org.tmatesoft.svn.core.wc.SVNWCUtil;
 
-public class SVNHistoryExtract {
+public class SVNHistoryExtract
+{
 
-	/**
-	 * @param args
-	 */
-	static SVNClientManager ourClientManager;
+    /**
+     * @param args
+     */
+    static SVNClientManager ourClientManager;
 
-public static void main(String[] args)throws Exception {
-	
-	String username="";
-	String password="";
-	Properties prop=new Properties();
-	String confPath="src/main/resources/config.properties";
-	try{
-		FileInputStream fis=new FileInputStream(confPath);
-		prop.load(fis);
-		}catch(Exception e){System.out.println("Error loading file: config.properties");}
-		username=prop.getProperty("username");
-		password=prop.getProperty("password");
-	String sheetName="logs";
-	String loc=JOptionPane.showInputDialog("Enter SVN Location");
-	String fileName=JOptionPane.showInputDialog("Enter Output FileName with .xls extension");
-	SVNURL url = SVNURL.parseURIDecoded( loc);
-	
-	File f=new File(fileName);
-	f.createNewFile();
-	WorkbookSettings wbSettings = new WorkbookSettings();
-    wbSettings.setLocale(new Locale("en", "EN"));
-	WritableWorkbook copy=Workbook.createWorkbook(f, wbSettings);
-	
-	WritableSheet sheetN = copy.createSheet(sheetName,0 );
-	int row=0;
-	int col=0;
-	WritableCell cell = sheetN.getWritableCell(1, 2);
-	Label label1 = new Label(col, row, "revision");
-	col++;
-	Label label2 = new Label(col, row, "author");
-	col++;
-	Label label3 = new Label(col, row, "date");
-	col++;
-	Label label4 = new Label(col, row, "log message");
-	col++;
-	Label label5 = new Label(col, row, "changed paths:");
-	col++;
-	sheetN.addCell(label1);
-	sheetN.addCell(label2);
-	sheetN.addCell(label3);
-	sheetN.addCell(label4);
-	sheetN.addCell(label5);
-	
-	SVNRepository repository = SVNRepositoryFactory.create(url);
-	ISVNAuthenticationManager authManager = SVNWCUtil.createDefaultAuthenticationManager(username, password);
-	repository.setAuthenticationManager(authManager);
-	Collection logEntries = repository.log(new String[] { "" }, null, 0, -1, true, true);
-	//System.out.println(logEntries);
-	int sheetnum=1;
-	for ( Iterator entries = logEntries.iterator( ); entries.hasNext( ); ) {
-        row++;
-        col=0;
-        if(row>60000)
-        { 
-        	sheetN = copy.createSheet(sheetName+sheetnum,sheetnum );
-        	row=1;
+    public static void main(String[] args)
+        throws Exception
+    {
+
+        String username = "";
+        String password = "";
+        Properties prop = new Properties();
+        String confPath = "src/main/resources/config.properties";
+        try
+        {
+            FileInputStream fis = new FileInputStream(confPath);
+            prop.load(fis);
         }
-		SVNLogEntry logEntry = ( SVNLogEntry ) entries.next( );
-        //System.out.println( "---------------------------------------------" );
-        //System.out.println ("revision: " + logEntry.getRevision( ) );
-        Label ilabel1 = new Label(col, row, logEntry.getRevision( )+"");
-        col++;
-        //System.out.println( "author: " + logEntry.getAuthor( ) );
-        Label ilabel2 = new Label(col, row, logEntry.getAuthor( )+"");
-        col++;
-        //System.out.println( "date: " + logEntry.getDate( ) );
-        Label ilabel3= new Label(col, row, logEntry.getDate( )+"");
-        col++;
-        //System.out.println( "log message: " + logEntry.getMessage( ) );
-        Label ilabel4= new Label(col, row, logEntry.getMessage( )+"");
-        col++;
-        String chngd="";
-        if ( logEntry.getChangedPaths( ).size( ) > 0 ) {
-            //System.out.println( );
-            //System.out.println( "changed paths:" );
-        	chngd="";
-            Set changedPathsSet = logEntry.getChangedPaths( ).keySet( );
+        catch (Exception e)
+        {
+            System.out.println("Error loading file: config.properties");
+        }
+        username = prop.getProperty("username");
+        password = prop.getProperty("password");
+        boolean accessOk = false;
 
-            for ( Iterator changedPaths = changedPathsSet.iterator( ); changedPaths.hasNext( ); ) {
-                SVNLogEntryPath entryPath = ( SVNLogEntryPath ) logEntry.getChangedPaths( ).get( changedPaths.next( ) );
-                
-                if(!(entryPath.getPath().contains("1769")))
-                	continue;
-                
-                chngd=( " "
-                        + entryPath.getType( )
-                        + " "
-                        + entryPath.getPath( )
-                        + ( ( entryPath.getCopyPath( ) != null ) ? " (from "
-                                + entryPath.getCopyPath( ) + " revision "
-                                + entryPath.getCopyRevision( ) + ")" : "" ) +"\n");
-                Label ilabel5= new Label(col, row, chngd);
-                sheetN.addCell(ilabel5);
-                row++;
+        String loc = JOptionPane.showInputDialog("Enter SVN Location");
+        String sender = JOptionPane.showInputDialog("Enter sender's name");
+        String version = JOptionPane.showInputDialog("Enter version name");
+        String releaseVersion = JOptionPane.showInputDialog("Enter release version");
+        String firstDay = JOptionPane.showInputDialog("Enter first day  of history (dd/mm/yy)");
+        String lastDay = JOptionPane.showInputDialog("Enter last  day  of history (dd/mm/yy)");
+
+        List<String> revisions = new ArrayList<String>();
+        List<String> commits = new ArrayList<String>();
+        SVNURL url = SVNURL.parseURIDecoded(loc);
+        SVNRepository repository = null;
+        repository = SVNRepositoryFactory.create(url);
+        ISVNAuthenticationManager authManager = SVNWCUtil.createDefaultAuthenticationManager(username, password);
+        repository.setAuthenticationManager(authManager);
+        Collection logEntries = repository.log(new String[] { "" }, null, 0, -1, true, true);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-mm-yy");
+        LocalDateTime now = LocalDateTime.now();
+        PrintWriter writer =
+            new PrintWriter("livraison_" + version + "_" + String.valueOf(dtf.format(now)) + ".txt", "UTF-8");
+        writer.println("Bonjour,\n");
+        writer.println("Vous trouverez ci-dessous le contenu de la livraison de la version " + version);
+        writer.println("Livraison " + version + "// à mettre en gras\n");
+        writer.println("La nouvelle version de l’IHM EPE " + version + " (" + releaseVersion
+            + ") est disponible sous le tag suivant : \n");
+
+        for (Iterator entries = logEntries.iterator(); entries.hasNext();)
+        {
+            SVNLogEntry logEntry = (SVNLogEntry) entries.next();
+            SimpleDateFormat formatter1 = new SimpleDateFormat("dd/MM/yy");
+            Date dateRefDebut = formatter1.parse(firstDay);
+            Date dateRefFin = formatter1.parse(lastDay);
+            if ((logEntry.getDate().after(dateRefDebut) || logEntry.getDate().equals(dateRefDebut))
+                && (logEntry.getDate().before(dateRefFin) || logEntry.getDate().equals(dateRefFin)))
+            {
+                revisions.add(String.valueOf(logEntry.getRevision()));
+                commits.add(logEntry.getMessage());
+                System.out.println("Révision : " + logEntry.getRevision() + "\t" + logEntry.getMessage());
             }
-            
         }
-        //Label ilabel5= new Label(col, row, chngd);
-        sheetN.addCell(ilabel1);
-    	sheetN.addCell(ilabel2);
-    	sheetN.addCell(ilabel3);
-    	sheetN.addCell(ilabel4);
-    	//sheetN.addCell(ilabel5);
-    	
+
+        String allRev = "";
+        for (String str : revisions)
+        {
+            allRev += str + " ";
+        }
+        writer.println("Révisions : " + allRev);
+        writer.println("");
+        writer.println("La livraison contient les correctifs suivants :");
+        writer.println("");
+        for (String str : commits)
+        {
+            if (str.contains("[DIVERS]"))
+            {
+                writer.println("[Technologie : Java] " + str);
+            }
+            else
+            {
+                writer.println("[Technologie : Java][DALIXXX_IRM-XXX] " + str);
+            }
+        }
+        writer.println("");
+        writer.println("La livraison contient les correctifs de la version précedente suivants :\n");
+        writer.println(sender);
+        writer.close();
+        JOptionPane.showMessageDialog(null, "Done");
     }
-	copy.write();
-	copy.close();
-	//workbook.close();
-//System.out.println("done");
-	JOptionPane.showMessageDialog(null, "Done");
-}
 }
